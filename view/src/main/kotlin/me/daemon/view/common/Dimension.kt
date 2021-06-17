a7d8706460159e6891f86eb676eb2630b7959779
daemon369 <daemon336699@gmail.com>
2021-06-17 12:52:49 +0800

    reorganize source code

--- view/src/main/kotlin/me/daemon/view/common/Dimension.kt
+++ view/src/main/kotlin/me/daemon/view/common/Dimension.kt
@@ -13,44 +13,45 @@ import androidx.fragment.app.Fragment
 inline fun Resources.dp2px(dp: Float): Float =
     TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics)
 
-inline fun Context.dp2px(dp: Float): Float = resources.dp2px(dp)
-
-inline fun View.dp2px(dp: Float): Float = resources.dp2px(dp)
-
-inline fun Fragment.dp2px(dp: Float): Float = resources.dp2px(dp)
-
-inline fun Dialog.dp2px(dp: Float): Float = context.dp2px(dp)
-
-
 inline fun Resources.px2dp(px: Float): Float = px / displayMetrics.density
-
-inline fun Context.px2dp(px: Float): Float = resources.px2dp(px)
-
-inline fun View.px2dp(px: Float): Float = resources.px2dp(px)
-
-inline fun Fragment.px2dp(px: Float): Float = resources.px2dp(px)
-
-inline fun Dialog.px2dp(px: Float): Float = context.px2dp(px)
-
 
 inline fun Resources.sp2px(sp: Float): Float =
     TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, displayMetrics)
 
-inline fun Context.sp2px(sp: Float): Float = resources.sp2px(sp)
-
-inline fun View.sp2px(sp: Float): Float = resources.sp2px(sp)
-
-inline fun Fragment.sp2px(sp: Float): Float = resources.sp2px(sp)
-
-inline fun Dialog.sp2px(sp: Float): Float = context.sp2px(sp)
-
-
 inline fun Resources.px2sp(px: Float): Float = px / displayMetrics.scaledDensity
+
+
+inline fun Context.dp2px(dp: Float): Float = resources.dp2px(dp)
+
+inline fun Context.px2dp(px: Float): Float = resources.px2dp(px)
+
+inline fun Context.sp2px(sp: Float): Float = resources.sp2px(sp)
 
 inline fun Context.px2sp(px: Float): Float = resources.px2sp(px)
 
+
+inline fun View.dp2px(dp: Float): Float = resources.dp2px(dp)
+
+inline fun View.px2dp(px: Float): Float = resources.px2dp(px)
+
+inline fun View.sp2px(sp: Float): Float = resources.sp2px(sp)
+
 inline fun View.px2sp(px: Float): Float = resources.px2sp(px)
 
+
+inline fun Fragment.dp2px(dp: Float): Float = resources.dp2px(dp)
+
+inline fun Fragment.px2dp(px: Float): Float = resources.px2dp(px)
+
+inline fun Fragment.sp2px(sp: Float): Float = resources.sp2px(sp)
+
 inline fun Fragment.px2sp(px: Float): Float = resources.px2sp(px)
+
+
+inline fun Dialog.dp2px(dp: Float): Float = context.dp2px(dp)
+
+inline fun Dialog.px2dp(px: Float): Float = context.px2dp(px)
+
+inline fun Dialog.sp2px(sp: Float): Float = context.sp2px(sp)
 
 inline fun Dialog.px2sp(px: Float): Float = context.px2sp(px)
--- view/src/main/kotlin/me/daemon/view/common/Util.kt
+++ view/src/main/kotlin/me/daemon/view/common/Util.kt
@@ -7,22 +7,10 @@ import android.content.Context
  * @since 2019-06-27 11:17
  */
 
-fun dp2px(context: Context, dp: Float): Float {
-    val density = context.resources.displayMetrics.density
-    return density * dp
-}
+fun dp2px(context: Context, dp: Float): Float = context.dp2px(dp)
 
-fun px2dp(context: Context, px: Float): Float {
-    val density = context.resources.displayMetrics.density
-    return px / density
-}
+fun px2dp(context: Context, px: Float): Float = context.px2dp(px)
 
-fun sp2px(context: Context, sp: Float): Float {
-    val scaleDensity = context.resources.displayMetrics.scaledDensity
-    return scaleDensity * sp
-}
+fun sp2px(context: Context, sp: Float): Float = context.sp2px(sp)
 
-fun px2sp(context: Context, px: Float): Float {
-    val scaleDensity = context.resources.displayMetrics.scaledDensity
-    return px / scaleDensity
-}
+fun px2sp(context: Context, px: Float): Float = context.px2sp(px)
