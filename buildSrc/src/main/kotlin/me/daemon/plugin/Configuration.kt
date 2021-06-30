b5c272c9b7e30c1bd0f866a983cb380f53e6e6cc
daemon369 <daemon336699@gmail.com>
2021-06-30 18:03:46 +0800

    lower requirements

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -3,7 +3,7 @@ package me.daemon.plugin
 object Configuration {
     const val minSdkVersion = 19
     const val compileSdkVersion = 30
-    const val targetSdkVersion = 29
+    const val targetSdkVersion = 23
     const val buildToolsVersion = "30.0.3"
     const val groupId = "io.github.daemon369"
     const val artifactId = "android-view-common"
