cf2f9b35fcdef2889e912b043392e913796caf44
daemon369 <daemon336699@gmail.com>
2021-09-05 09:51:06 +0800

    upgrade libraries

--- buildSrc/src/main/kotlin/me/daemon/plugin/Libraries.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Libraries.kt
@@ -7,16 +7,17 @@ object Libraries {
      */
     @Suppress("ClassName")
     private object v {
-        const val kotlin = "1.4.32"
-        const val coroutines = "1.4.2"
+        const val kotlin = "1.5.21"
+        const val coroutines = "1.5.1"
 
-        const val core = "1.5.0"
-        const val appCompat = "1.3.0"
+        const val annotation = "1.2.0"
+        const val core = "1.6.0"
+        const val appCompat = "1.3.1"
         const val lifecycle = "2.3.1"
-        const val activity = "1.2.3"
-        const val fragment = "1.3.4"
+        const val activity = "1.3.1"
+        const val fragment = "1.3.6"
         const val navigation = "2.3.5"
-        const val constraintLayout = "2.0.4"
+        const val constraintLayout = "2.1.0"
 
         const val gradlePlugin = "7.0.2"
         const val junit = "4.13.2"
