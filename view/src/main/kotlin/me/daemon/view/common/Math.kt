2371b598df2f42ec85458c8f43548af68dd9cf12
daemon369 <daemon336699@gmail.com>
2021-09-05 10:02:56 +0800

    remove useless annotations

--- view/src/main/kotlin/me/daemon/view/common/Math.kt
+++ view/src/main/kotlin/me/daemon/view/common/Math.kt
@@ -9,10 +9,6 @@ inline fun between(min: Double, max: Double, value: Double): Double = max(min, m
 inline fun between(min: Float, max: Float, value: Float): Float = max(min, min(max, value))
 inline fun between(min: Int, max: Int, value: Int): Int = max(min, min(max, value))
 inline fun between(min: Long, max: Long, value: Long): Long = max(min, min(max, value))
-
-@ExperimentalUnsignedTypes
 inline fun between(min: UInt, max: UInt, value: UInt): UInt = max(min, min(max, value))
-
-@ExperimentalUnsignedTypes
 inline fun between(min: ULong, max: ULong, value: ULong): ULong = max(min, min(max, value))
 
