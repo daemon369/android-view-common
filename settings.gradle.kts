6eb8b073808e5263e82dcdbdd519d125095929a4
daemon369 <daemon336699@gmail.com>
2021-11-14 00:14:27 +0800

    upgrade version catalog library

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("io.github.daemon369:android-version-catalog:0.0.3")
+            from("io.github.daemon369:android-version-catalog:0.0.4")
         }
     }
 }
