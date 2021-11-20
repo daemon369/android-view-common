a7d8dd711e89aa92299d3b8b0734105bb27d9e4d
daemon369 <daemon336699@gmail.com>
2021-11-21 00:52:05 +0800

    upgrade compileSdkVersion & buildToolsVersion

--- buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
+++ buildSrc/src/main/kotlin/me/daemon/plugin/Configuration.kt
@@ -2,9 +2,9 @@ package me.daemon.plugin
 
 object Configuration {
     const val minSdkVersion = 19
-    const val compileSdkVersion = 30
+    const val compileSdkVersion = 31
     const val targetSdkVersion = 23
-    const val buildToolsVersion = "30.0.3"
+    const val buildToolsVersion = "31.0.0"
     const val groupId = "io.github.daemon369"
     const val artifactId = "android-view-common"
     const val version = "0.3.0"
